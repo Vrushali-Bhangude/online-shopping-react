641878a8f55523e421a97264bfc6d039230b2d0f
Vrushali Bhangude <vrushalibhangude2@gmail.com>
2025-06-24 23:31:55 +0530

    navbar and home page added

--- online-shopping/src/App.jsx
+++ online-shopping/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import './App.css';
+import Nav from './components/Navbar/Nav';
+import Home from './views/Home/Home';
 const App = () => {
   return (
-    <div>
-      <h1>app</h1>
-    </div>
+     <>
+     <Nav />
+     <Home />
+     </>
   )
 }
 
