20028bf057c9795c1117fa0e60e908b1b535fada
KiranRaut2137 <rautkiran700@gmail.com>
2025-06-26 10:46:12 +0530

    signup page design added

--- online-shopping/src/App.jsx
+++ online-shopping/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './App.css';
-import Nav from './components/Navbar/Nav';
 import Home from './views/Home/Home';
-import Login from './views/Login/Login';
-import Signup from './views/Signup/Signup';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const App = () => {
   return (
      <>
-     <Nav />
-     <Home /> 
+      <BrowserRouter>
+          <Routes>
+             <Route path="/" element={<Home/>}/>
+          </Routes>
+      </BrowserRouter>
      </>
   )
 }
