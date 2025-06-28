4b7068af3898d425d9d031150ec660174729029d
Gangavane412 <gangawanepriyanka412@gmail.com>
2025-06-28 08:17:05 +0530

    adding kis updated  section

--- online-shopping/src/views/Kid/Kids.jsx
+++ online-shopping/src/views/Kid/Kids.jsx
@@ -2,30 +2,57 @@ import React from 'react';
 import './Kids.css';
 
 const kidsProducts = [
+ const kidsProducts = [
   {
     id: 1,
     name: 'Cute Teddy Bear',
-    image: '/images/teddy.jpg',
+    image: 'https://m.media-amazon.com/images/I/61a+GEvULPL._AC_UL480_FMwebp_QL65_.jpg',
     price: 499,
   },
   {
     id: 2,
     name: 'Kids Sneakers',
-    image: '/images/sneakers.jpg',
+    image: 'https://m.media-amazon.com/images/I/71RZ5qDY03L._UL1500_.jpg',
     price: 999,
   },
   {
     id: 3,
     name: 'Cartoon T-Shirt',
-    image: '/images/tshirt.jpg',
+    image: 'https://m.media-amazon.com/images/I/71vFKBpKakL._UL1500_.jpg',
     price: 299,
   },
   {
     id: 4,
     name: 'Learning Puzzle',
-    image: '/images/puzzle.jpg',
+    image: 'https://m.media-amazon.com/images/I/81UwnLWdLaL._SL1500_.jpg',
     price: 349,
   },
+  {
+    id: 5,
+    name: 'Kids Backpack - Unicorn',
+    image: 'https://m.media-amazon.com/images/I/71X+7X1JeaL._SL1500_.jpg',
+    price: 699,
+  },
+  {
+    id: 6,
+    name: 'Remote Control Car',
+    image: 'https://m.media-amazon.com/images/I/71N1n-j1yNL._SL1500_.jpg',
+    price: 1299,
+  },
+  {
+    id: 7,
+    name: 'Baby Girl Dress',
+    image: 'https://m.media-amazon.com/images/I/71w4bW2TsmL._UL1500_.jpg',
+    price: 799,
+  },
+  {
+    id: 8,
+    name: 'Stuffed Bunny Plush Toy',
+    image: 'https://m.media-amazon.com/images/I/71xQvF1SrxL._SL1500_.jpg',
+    price: 449,
+  }
+];
+
 ];
 
 function Kids() {
